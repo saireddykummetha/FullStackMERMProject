b540a94922d70b6fac0adb9afff706c47758473f
saireddykummetha <kummethasaikumarreddy@gmail.com>
2025-04-27 21:06:21 +0530

    Update Seacrchbar.jsx

--- frontend/src/Component/Pages/Seacrchbar.jsx
+++ frontend/src/Component/Pages/Seacrchbar.jsx
@@ -4,9 +4,7 @@ const Seacrchbar = ({loading}) => {
   
   return (
     <div>
-        {/* <div className=" py-5">
-            <h2 className="text-1xl font-bold p-10">Popular Categories in <span className='text-blue-700'>books</span></h2>
-            </div> */}
+      
             <div className='flex flex-wrap cursor-pointer gap-5 justify-center align-center px-10'>
             {
               homeproductcartlist[0] ? homeproductcartlist.map(el=>{
